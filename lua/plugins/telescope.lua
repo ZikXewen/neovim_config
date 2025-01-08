bb21bb2ae2db799cd037f2e366038f515e235009
ZikXewen <datasup12@gmail.com>
2025-01-07 20:26:46 -0600

    tweaks

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -18,9 +18,12 @@ return {
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<Leader>ff", builtin.find_files, { desc = "Find files" })
       vim.keymap.set("n", "<Leader>fh", builtin.help_tags, { desc = "Find help" })
-      vim.keymap.set("n", "<Leader>fw", builtin.live_grep, { desc = "Find words" })
-      vim.keymap.set("n", "<Leader>fW", function() builtin.live_grep({ glob_pattern = "!**/.git/*", additional_args = { "--hidden" }}) end, { desc = "Find words in all files" })
+      vim.keymap.set("n", "<Leader>f/", builtin.current_buffer_fuzzy_find, { desc = "Find words in current buffer" })
       vim.keymap.set("n", "<Leader>fc", function() builtin.find_files({ cwd = vim.fn.stdpath "config", follow = true }) end, { desc = "Find config" })
+      if vim.fn.executable "rg" == 1 then
+        vim.keymap.set("n", "<Leader>fw", builtin.live_grep, { desc = "Find words" })
+        vim.keymap.set("n", "<Leader>fW", function() builtin.live_grep({ glob_pattern = "!**/.git/*", additional_args = { "--hidden" }}) end, { desc = "Find words in all files" })
+      end
     end
   }
 }
