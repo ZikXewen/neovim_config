aa8a118a1e6dd77cb93c27894640937d3cb4dd2a
ZikXewen <datasup12@gmail.com>
2025-01-07 20:08:51 -0600

    change keymaps

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -10,13 +10,13 @@ return {
     local cmp = require("cmp")
     local lspkind = require("lspkind")
     cmp.setup({
-      mapping = cmp.mapping.preset.insert({
-        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      mapping = {
         ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.close(),
-        ["<CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })
-      }),
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<Tab>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })
+      },
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
         { name = "buffer" },
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,6 +5,18 @@ return {
     local oil = require("oil")
     oil.setup({
       skip_confirm_for_simple_edits = true,
+      use_default_keymaps = false,
+      keymaps = {
+        ["<C-r>"] = "actions.refresh",
+        ["<CR>"] = { "actions.select", mode = "n" },
+        ["<BS>"] = { "actions.parent", mode = "n" },
+        ["q"] = { "actions.close", mode = "n" },
+        ["p"] = { "actions.preview", mode = "n" },
+        ["`"] = { "actions.cd", mode = "n" },
+        ["h"] = { "actions.toggle_hidden", mode = "n" },
+        ["gx"] = { "actions.open_external", mode = "n" },
+        ["?"] = { "actions.show_help", mode = "n" },
+      },
       view_options = {
         show_hidden = true,
         is_always_hidden = function(name, _)
@@ -12,6 +24,6 @@ return {
         end
       }
     })
-    vim.keymap.set("n", "-", oil.toggle_float, {})
+    vim.keymap.set("n", "<BS>", oil.toggle_float, {})
   end
 }
